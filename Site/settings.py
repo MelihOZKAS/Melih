669ea2117d7d0402c199fb6bbcd232545060ee29
MelihOZKAS <melihozkass@gmail.com>
2023-04-19 18:55:37 +0300

    feat: aws özelliği kesildi

--- Site/settings.py
+++ Site/settings.py
@@ -124,45 +124,50 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-if DEBUG:
-    STATIC_URL = "/static/"
-    STATIC_ROOT = BASE_DIR / "staticfiles"
-
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
-
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-else:
-    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
-
-
-
-    DEFAULT_FILE_STORAGE = 'Site.costum_storages.MediaStorage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
-    }
-
-    STATIC_URL = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
-    STATIC_ROOT = STATIC_URL
-    MEDIA_LOCATION = 'media'
-    Resim_kayit_yeri = MEDIA_LOCATION + '/resimler'
-    Dosya_kayit_yeri = MEDIA_LOCATION + '/dosyalar'
+#if DEBUG:
+#    STATIC_URL = "/static/"
+#    STATIC_ROOT = BASE_DIR / "staticfiles"
+#
+#    MEDIA_URL = "/media/"
+#    MEDIA_ROOT = BASE_DIR / "media"
+#
+#    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+#else:
+#    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+#    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+#    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+#    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+#
+#
+#
+#    DEFAULT_FILE_STORAGE = 'Site.costum_storages.MediaStorage'
+#    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#
+#
+#    AWS_DEFAULT_ACL = 'public-read'
+#    AWS_S3_OBJECT_PARAMETERS = {
+#        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#        'CacheControl': 'max-age=94608000'
+#    }
+#
+#    STATIC_URL = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+#    STATIC_ROOT = STATIC_URL
+#    MEDIA_LOCATION = 'media'
+#    Resim_kayit_yeri = MEDIA_LOCATION + '/resimler'
+#    Dosya_kayit_yeri = MEDIA_LOCATION + '/dosyalar'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
