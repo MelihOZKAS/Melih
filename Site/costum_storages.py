80f92605b08021a94a9a0c26f9f6a5d0c4eca331
MelihOZKAS <melihozkass@gmail.com>
2023-04-19 12:07:40 +0300

    feat: yeni düzenlemeler

--- Site/costum_storages.py
+++ Site/costum_storages.py
@@ -12,7 +12,6 @@ if settings.DEBUG:
 else:
     from storages.backends.s3boto3 import S3Boto3Storage
 
-
     class MediaStorage(S3Boto3Storage):
         location = settings.MEDIA_LOCATION
         file_overwrite = False
