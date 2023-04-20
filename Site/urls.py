c694de8bd0588086f896623968383b8fa85da075
MelihOZKAS <melihozkass@gmail.com>
2023-04-21 01:34:08 +0300

    feat: Düzeltme Sunucu kontor.urls lere açıldı

--- Site/urls.py
+++ Site/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include("Site.urls")),
+    path('',include("kontor.urls")),
     path("admin/", admin.site.urls),
 ]
 
