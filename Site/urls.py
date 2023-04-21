7c5026640a21aa3c43fa2dce7417aac41a671c5e
MelihOZKAS <melihozkass@gmail.com>
2023-04-22 02:41:22 +0300

    feat:  Numara = models.BigIntegerField() güncellendi.

--- Site/urls.py
+++ Site/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include("kontor.urls")),
+    path('bayi/',include("kontor.urls")),
     path("admin/", admin.site.urls),
 ]
 
