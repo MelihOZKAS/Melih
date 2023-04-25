dd764e3a5863a43c7a1aee0c090b0a2333213642
MelihOZKAS <melihozkass@gmail.com>
2023-04-25 20:08:30 +0300

    feat: default=None

--- kontor/migrations/0002_siparisler_user.py
+++ kontor/migrations/0002_siparisler_user.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='siparisler',
             name='user',
-            field=models.ForeignKey(default=django.utils.timezone.now, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
