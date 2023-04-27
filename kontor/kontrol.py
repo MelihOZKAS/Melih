6dac9fb7b042548e52bf004fd28d03248afd0027
MelihOZKAS <melihozkass@gmail.com>
2023-04-28 02:19:19 +0300

    feat: küçük bir hata düzeltildi +**{'değişti1 baya }+ güncellendi

--- kontor/kontrol.py
+++ kontor/kontrol.py
@@ -1020,25 +1020,27 @@ def VodafonePaketleriCek(request):
                 KategorisiGelen = Kategori.objects.get(pk=3)
                 api1 = Apiler.objects.get(pk=3)
                 api2 = Apiler.objects.get(pk=2)
-                kontor_obj = KontorList(
+                obj, created = KontorList.objects.get_or_create(
                     Kupur=paketID,
-                    Urun_adi=Paket,
-                    Urun_Detay=Paket,
-                    GunSayisi=paketDay,
-                    MaliyetFiyat=paketFiyat,
-                    SatisFiyat=Decimal(int(paketFiyat) + 5),
-                    HeryoneDK=paketDK,
-                    Sebekeici=Decimal('0.00'),
-                    internet=Decimal(str(int(paketGB) * 1000)),
-                    SMS=paketSMS,
-                    YurtDisiDk=Decimal('0.00'),
-                    Aktifmi=True,
-                    Kategorisi=KategorisiGelen,
-                    api1=api1,
-                    api2=api2,
-                    zNetKupur=znetFix
+                    defaults={
+                        "Urun_adi": Paket,
+                        "Urun_Detay": Paket,
+                        "Kupur": paketID,
+                        "GunSayisi": paketDay,
+                        "MaliyetFiyat": paketFiyat,
+                        "SatisFiyat": Decimal(int(paketFiyat) + 5),
+                        "HeryoneDK": paketDK,
+                        "Sebekeici": Decimal('0.00'),
+                        "internet": Decimal(str(int(paketGB) * 1000)),
+                        "SMS": paketSMS,
+                        "YurtDisiDk": Decimal('0.00'),
+                        "Aktifmi": True,
+                        "Kategorisi": KategorisiGelen,
+                        "api1": api1,
+                        "api2": api2,
+                        "zNetKupur": znetFix
+                    }
                 )
-                kontor_obj.save()
             except Exception as e:
                 print(f'Hata: {e}')
 
