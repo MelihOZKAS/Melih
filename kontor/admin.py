7f88848874b6ee2fc21ba3e1a7a8f0328433f4d3
MelihOZKAS <melihozkass@gmail.com>
2023-05-05 00:43:58 +0300

    feat: ApidenCekilenPaketler Sisteme Çekilmeis NASİPSE v21

--- kontor/admin.py
+++ kontor/admin.py
@@ -253,6 +253,34 @@ class AdminKategoriListesi(admin.ModelAdmin):
 
 
 
+#class VodafoneSesInlineForm(forms.ModelForm):
+#    class Meta:
+#        model = VodafonePaketler
+#        exclude = []
+#
+#    def __init__(self, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#
+#        apiden_gelenler = self.fields['Apiden_gelenler']
+#        apiden_gelenler.empty_label = '-Paket Seçiniz.'
+#
+#        if self.instance and self.instance.eslestirme_kupur:
+#            apiden_gelenler.queryset = apiden_gelenler.queryset.filter(kupur=self.instance.eslestirme_kupur)
+#            apiden_gelenler.empty_label = None
+#
+#        apiden_gelenler.label_from_instance = lambda obj: f"{obj.urun_adi} ({obj.kupur})"
+#
+#    def save(self, commit=True):
+#        vodafone_paketler = super().save(commit=False)
+#        if vodafone_paketler.Apiden_gelenler:
+#            apiden_gelenler = vodafone_paketler.Apiden_gelenler
+#            eslestirme_kupur = apiden_gelenler.kupur
+#            api_gelen_fiyat = apiden_gelenler.ApiGelen_fiyati
+#            vodafone_paketler.eslestirme_kupur = eslestirme_kupur
+#            vodafone_paketler.alis_fiyati = api_gelen_fiyat
+#        if commit:
+#            vodafone_paketler.save()
+#        return vodafone_paketler
 class VodafoneSesInlineForm(forms.ModelForm):
     class Meta:
         model = VodafonePaketler
@@ -273,7 +301,7 @@ class VodafoneSesInlineForm(forms.ModelForm):
     def save(self, commit=True):
         vodafone_paketler = super().save(commit=False)
         if vodafone_paketler.Apiden_gelenler:
-            apiden_gelenler = vodafone_paketler.Apiden_gelenler
+            apiden_gelenler = ApidenCekilenPaketler.objects.get(id=vodafone_paketler.Apiden_gelenler.id, apiler=vodafone_paketler.apiler)
             eslestirme_kupur = apiden_gelenler.kupur
             api_gelen_fiyat = apiden_gelenler.ApiGelen_fiyati
             vodafone_paketler.eslestirme_kupur = eslestirme_kupur
