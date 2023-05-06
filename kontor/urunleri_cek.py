f663667dcf3956a73099e61830dedf07bd017354
MelihOZKAS <melihozkass@gmail.com>
2023-05-06 23:58:03 +0300

    feat: ANa Operator ve Alt Operatorleri Ekleme eklendi. v2

--- kontor/urunleri_cek.py
+++ kontor/urunleri_cek.py
@@ -1,5 +1,5 @@
 import requests
-from .models import Apiler, ApidenCekilenPaketler
+from .models import Apiler, ApidenCekilenPaketler,AnaOperator,AltOperator
 from decimal import Decimal
 import json
 
