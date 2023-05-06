fd1f6c75f6681a834702ddd2d0ebf9650ecb164c
MelihOZKAS <melihozkass@gmail.com>
2023-05-06 19:50:00 +0300

    feat: bazi buglar düzeltildi. v9

--- kontor/admin.py
+++ kontor/admin.py
@@ -327,7 +327,7 @@ class VodafoneSesInlineForm(forms.ModelForm):
         apiden_gelenler.empty_label = '-Paket Seçiniz.'
 
         if self.instance and self.instance.eslestirme_kupur:
-            apiden_gelenler.queryset = apiden_gelenler.queryset.filter(kupur=self.instance.eslestirme_kupur, apiler=self.instance.apiler)
+            apiden_gelenler.queryset = apiden_gelenler.queryset.filter(apiler=self.instance.apiler)
             #apiden_gelenler.empty_label = None
         else:
             apiden_gelenler.queryset = apiden_gelenler.queryset.none()
