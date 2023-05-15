395a8323ab526938820c7f054b0d44d20be55a0f
MelihOZKAS <melihozkass@gmail.com>
2023-05-15 22:44:26 +0300

    feat: api degis v23

--- kontor/admin.py
+++ kontor/admin.py
@@ -44,18 +44,13 @@ class AdminApidenCekilenPaketler(admin.ModelAdmin):
 from django.shortcuts import render
 
 def update_api1_with_selected_api(modeladmin, request, queryset):
-    if request.POST.get('post'):
-        selected_api = request.POST.get('selected_api')
-        queryset.update(api1_id=selected_api)
+
+    formset = SelectAPIForm(request.POST or None, queryset=queryset)
+    if request.method == 'POST' and formset.is_valid():
+        selected_api = formset.cleaned_data[0]['api_choice']
+        queryset.update(api1=selected_api)
         return None
 
-    SelectAPIFormSet = modelformset_factory(
-        KontorList,
-        fields=('api1',),
-        extra=0,
-        widgets={'api1': forms.Select(choices=Apiler.objects.values_list('id', 'Apiadi'))}
-    )
-    formset = SelectAPIFormSet(queryset=queryset)
     return render(request, "select_api_form.html", {"formset": formset})
 
 
