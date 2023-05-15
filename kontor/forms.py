f9a5fbfaeadb7c9eabcd317f2c6f39fa974bb9b7
MelihOZKAS <melihozkass@gmail.com>
2023-05-16 00:45:42 +0300

    feat: api degis v46

--- kontor/forms.py
+++ kontor/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Apiler
 
 class SelectAPIForm(forms.Form):
-    apiler = [(api.id, api.name) for api in Apiler.objects.all()]
+    apiler = [(api.id, str(api)) for api in Apiler.objects.all()]
     selected_api = forms.ChoiceField(label="Selected API", choices=apiler, required=True)
+
 
