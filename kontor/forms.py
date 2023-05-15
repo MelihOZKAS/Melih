32c00093bfb88e0d8d75340e61962ab88d773948
MelihOZKAS <melihozkass@gmail.com>
2023-05-16 01:01:54 +0300

    feat: api degis v50

--- kontor/forms.py
+++ kontor/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Apiler
 
 class SelectAPIForm(forms.Form):
-    selected_api = forms.ChoiceField(label="Selected API", required=True)
+    selected_api = forms.ChoiceField(label="Selected APIII", required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
