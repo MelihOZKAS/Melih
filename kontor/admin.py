36d9abe965d3e82e2c48698d9674bcf5c5359405
MelihOZKAS <melihozkass@gmail.com>
2023-05-17 02:52:25 +0300

    feat: api degis v143

--- kontor/admin.py
+++ kontor/admin.py
@@ -71,16 +71,18 @@ class AdminKontorListesi(admin.ModelAdmin):
     actions = ['otoyap_action','TumAlternetifiSil_action',"change_api"]
 
     def change_api(self, request, queryset):
-        self.message_user(request, f"Buraya....")
-        if 'apply' in request.POST:
-            self.message_user(request, f"Buraya Geldim1")  # Debug: request.POST bilgisini yazdır.
+        self.message_user(request, f"Debug: request.queryset: {queryset}")  # Debug: request.POST bilgisini yazdır
+        form = None
+        if request.method == 'POST':
+            self.message_user(request, f"Buraya Geldim1")  # Debug: request.POST bilgisini yazdır
             form = ApiForm(request.POST)
             if form.is_valid():
-                self.message_user(request, f"Buraya Geldim2")  # Debug: request.POST bilgisini yazdır.
+                self.message_user(request, f"Buraya Geldim2")  # Debug: request.POST bilgisini yazdır
                 api1 = form.cleaned_data['api1']
                 api2 = form.cleaned_data.get('api2')  # get methodunu kullanarak None dönebilir
                 api3 = form.cleaned_data.get('api3')  # get methodunu kullanarak None dönebilir
-
+                self.message_user(request,
+                                  f"Debug: form is valid, api1: {api1}, api2: {api2}, api3: {api3}")  # Debug: Formun geçerli olduğunu ve api bilgisini yazdır
                 update_fields = {'api1': api1}
                 if api2 is not None:
                     update_fields['api2'] = api2
@@ -88,11 +90,13 @@ class AdminKontorListesi(admin.ModelAdmin):
                     update_fields['api3'] = api3
                 queryset.update(**update_fields)
                 self.message_user(request, f"API'ler Başarıyla Güncellendi! API1: {api1}, API2: {api2}, API3: {api3}")
-                return HttpResponseRedirect(request.get_full_path())
-        else:
-            self.message_user(request, f"direkGeldim")
+                return None
+            else:
+                messages.error(request, f'Form geçerli değil, lütfen tekrar deneyin. Post verisi: {request.POST}')
+                messages.error(request, f'Form hataları: {form.errors}')
+        if not form:
             form = ApiForm(initial={'selected_items': queryset.values_list('id', flat=True)})
-            return render(request, 'change_api.html', {'form': form, 'queryset': queryset})
+        return render(request, 'change_api.html', {'form': form, 'queryset': queryset})
 
     change_api.short_description = "API Değiştir"
     def otoyap_action(self, request, queryset):
