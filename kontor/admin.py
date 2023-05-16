6202160b1c28018331f41d48bcd1238549f3ec87
MelihOZKAS <melihozkass@gmail.com>
2023-05-16 11:32:19 +0300

    feat: api degis v96

--- kontor/admin.py
+++ kontor/admin.py
@@ -59,12 +59,16 @@ class AdminKontorListesi(admin.ModelAdmin):
     def update_api1_with_selected_api_view(self, request, queryset):
         if request.method == 'POST':
             form = SelectAPIForm(request.POST)
+            if form.is_valid():
+                selected_api = form.cleaned_data['selected_api']
+                apisi = Apiler.objects.get(pk=selected_api.id)
 
-            selected_api = form.cleaned_data['selected_api']
-            apisi = Apiler.objects.get(pk=selected_api.id)
-            queryset.update(api1=apisi)
-            self.message_user(request, "API güncelleme başarılı!", messages.SUCCESS)
-            return
+
+                queryset.update(api1=apisi)
+                self.message_user(request, "API güncelleme başarılı!", messages.SUCCESS)
+                return
+            else:
+                messages.alert(request, form.errors)
         else:
             form = SelectAPIForm()
 
