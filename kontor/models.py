77d99f2c3a6bace1c9bc64f30225f63c5732e9ef
MelihOZKAS <melihozkass@gmail.com>
2023-05-30 14:53:29 +0300

    feat: özel apiyi çekme Nasipse v3

--- kontor/models.py
+++ kontor/models.py
@@ -403,7 +403,7 @@ class Bayi_Listesi(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     Fiyati = models.OneToOneField(FiyatGuruplari, on_delete=models.CASCADE,blank=True,null=True)
     Bayi_Bakiyesi = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Borc = models.DecimalField(max_digits=10, decimal_places=2)
+    Borc = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Tutar = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     secili_banka = models.ForeignKey(Banka, on_delete=models.CASCADE,null=True,default=None)
     islem_durumu = models.CharField(max_length=20, choices=(
