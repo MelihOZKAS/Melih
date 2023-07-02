523e1538a1434a733dc5e3b9edbb7f1cf2307d7c
MelihOZKAS <melihozkass@gmail.com>
2023-07-02 14:48:04 +0300

    feat: test v2

--- Site/settings.py
+++ Site/settings.py
@@ -16,7 +16,6 @@ import environ
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 env = environ.Env(DEBUG=(bool,False))
-
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
