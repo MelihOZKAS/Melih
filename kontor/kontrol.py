44f9aa590d209c751a02fdbbcbc92ae9fd01df99
MelihOZKAS <melihozkass@gmail.com>
2024-01-27 10:48:50 +0300

    feat: duzeltme 3

--- kontor/kontrol.py
+++ kontor/kontrol.py
@@ -1032,7 +1032,7 @@ def AlternatifKontrol(request):
                 if alternative.product_id in PaketSorguListesi:
                     cikan_idler.append(alternative.product_id)
         except:
-            if siparis.SorguPaketID == "VodafoneDegilKi,GNC001" or siparis.SorguPaketID == "VodafoneDegilKi" or siparis.SorguPaketID == "TurkcellDegil" or siparis.SorguPaketID == "TurkcellDegil,GNC001" or siparis.SorguPaketID == "HatFaturali!" or siparis.SorguPaketID == "AveaDegil!" or siparis.SorguPaketID == "NumaraEksik!" or siparis.SorguPaketID == "[GirmişolduğunuznumaraTurkcell’denhizmetalmamaktadır.]":
+            if siparis.SorguPaketID == "VodafoneDegilKi,GNC001" or siparis.SorguPaketID == "VodafoneDegilKi" or siparis.SorguPaketID == "TurkcellDegil" or siparis.SorguPaketID == "TurkcellDegil,GNC001" or siparis.SorguPaketID == "HatFaturali!" or siparis.SorguPaketID == "AveaDegil!" or siparis.SorguPaketID == "NumaraEksik!" or siparis.SorguPaketID == "[GirmişolduğunuznumaraTurkcell’denhizmetalmamaktadır.]" or siparis.SorguPaketID == "AveaDeğilyadaBirikmişVergiBorcuVardırÖnceTLYüklemesiGerekir!":
                 siparis.Durum = iptalEdildi
                 siparis.SonucTarihi = timezone.now()
                 siparis.BayiAciklama = siparis.SorguPaketID
