7186f647bae2508069091686db01cb728cae4123
MelihOZKAS <melihozkass@gmail.com>
2024-05-06 23:56:55 +0300

    feat: duzeltme 3

--- kontor/views.py
+++ kontor/views.py
@@ -181,7 +181,7 @@ def SmsYakala(request):
 
         mesaj_yeni = mesaj.replace(",", "")
 
-        if gonderen == 'FUPS' and mesaj_ilk in ['3D', 'Son', 'İlk'] or mesaj_ilk in ['in', 'risini', "layabilirsin.", 'ni', 'Son', 'Sonu', 'İlk']:
+        if gonderen == 'FUPS' and mesaj_ilk in ['3D', 'Son', 'İlk'] or mesaj_ilk in ['in', 'risini', "mlayabilirsin.", "layabilirsin.", 'ni', 'Son', 'Sonu', 'İlk']:
             return HttpResponse("Basarili")
 
         else:
